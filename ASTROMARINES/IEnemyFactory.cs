8af164b0dd36357515a79f8664a63d6fa36173ac
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2017-03-15 23:15:24 +0100

    Created enemy factory.

---
+++ ASTROMARINES/IEnemyFactory.cs
@@ -0,0 +1,38 @@
+﻿using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+
+namespace ASTROMARINES
+{
+    interface IEnemyFactory
+    {
+        IEnemy CreateEnemy(EnemyTypes enemyType);
+    }
+
+    class EnemyFactory : IEnemyFactory
+    {
+        private List<Texture> enemyTextures;
+
+        public EnemyFactory(List<Texture> enemyTextures)
+        {
+            this.enemyTextures = enemyTextures;
+        }
+
+        public IEnemy CreateEnemy(EnemyTypes enemyType)
+        {
+            switch(enemyType)
+            {
+                case EnemyTypes.PowerUp:
+                    return new Enemy1(enemyTextures);
+                case EnemyTypes.Enemy2:
+                    throw new Exception("This enemy hasn't been implemented yet :D");
+                case EnemyTypes.Enemy3:
+                    throw new Exception("This enemy hasn't been implemented yet :D");
+                case EnemyTypes.Enemy4:
+                    throw new Exception("This enemy hasn't been implemented yet :D");
+                default:
+                    throw new Exception("You tried to create non-existing enemy");
+            }
+        }
+    }
+}
